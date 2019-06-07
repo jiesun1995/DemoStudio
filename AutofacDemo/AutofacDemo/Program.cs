b33d58e563d79664ed246bdff25452f63d6d6c95
Administrator <Administrator@DESKTOP-CMLRTU5>
2019-06-07 12:14:17 +0800

    same

--- AutofacDemo/AutofacDemo/Program.cs
+++ AutofacDemo/AutofacDemo/Program.cs
@@ -13,28 +13,22 @@ namespace AutofacDemo
         {
             var builder = new ContainerBuilder();//实例化 AutoFac  容器   
             var serviceDescriptors = new ServiceCollection();
-
-            
             var assemblys = AppDomain.CurrentDomain.GetAssemblies().ToList();//Service是继承接口的实现方法类库名称
             var baseType = typeof(IDependency);//IDependency 是一个接口（所有要实现依赖注入的借口都要继承该接口）
             builder.RegisterAssemblyTypes(assemblys.ToArray())
              .Where(m => baseType.IsAssignableFrom(m) && m != baseType)
              .AsImplementedInterfaces().InstancePerLifetimeScope();
-
-
             builder.RegisterAssemblyTypes(assemblys.ToArray())
                 .Where(x => x.GetInterfaces().Contains(baseType))
                 .AsSelf()
                 .InstancePerDependency();
             //builder.RegisterType<Startup>();
             builder.Populate(serviceDescriptors);
-            var ApplicationContainer = builder.Build();
+            var ApplicationContainer = new AutofacServiceProvider(builder.Build());
             //var ApplicationContainer= new AutofacServiceProvider(ApplicationContainer);
-
-
-
             //var testservice = ApplicationContainer.Resolve<ITestService>();
-            var startup = ApplicationContainer.Resolve<Startup>();
+            //var startup = ApplicationContainer.Resolve<Startup>();
+            var startup = ApplicationContainer.GetRequiredService<Startup>();
             startup.Run();
             //testservice.Test();
             Console.WriteLine("Hello World!");
