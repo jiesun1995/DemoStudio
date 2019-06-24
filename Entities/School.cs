56fbc64d0b6e7531db54f91a4bfc0f9b49310853
Administrator <Administrator@DESKTOP-CMLRTU5>
2019-06-24 10:08:52 +0800

    update

--- Entities/School.cs
+++ Entities/School.cs
@@ -5,8 +5,7 @@ using System.Text;
 namespace Entities
 {
     public class School:Entity
-    {
-        [Column("strFirstName")]
+    {        
         public override string Id { get => base.Id; set => base.Id = value; }
         public string Name { set; get; }
     }
--- dapperDemo/Program.cs
+++ dapperDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 
 namespace dapperDemo
 {
@@ -6,7 +7,16 @@ namespace dapperDemo
     {
         static void Main(string[] args)
         {
+            IPAddress[] addressList = Dns.GetHostEntry(Dns.GetHostName()).AddressList;
 
+            //如果本机IP列表小于2，则返回空字符串
+            if (addressList.Length < 2)
+            {
+                return ;
+            }
+
+            //返回本机的广域网IP
+            addressList[1].ToString();
 
 
             Console.WriteLine("Hello World!");
